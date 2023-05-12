f683ad28088262341effb985152b7b9d662f9bad
Rotnen Goyit <rotneng@gmail.com>
2023-05-12 08:03:36 +0100

    commit

---
+++ src/PhysicsCalculator.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PhysicsCalculator extends JFrame {
+    private JLabel problemLabel;
+    private JLabel resultLabel;
+
+    private JTextField inputField1;
+    private JTextField inputField2;
+
+    private JButton velocityButton;
+    private JButton forceButton;
+
+   
